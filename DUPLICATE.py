352f222260cd329fbc9eb53d1df8a6d8bf67d81b
Sankara96 <sankar.subramanian96@gmail.com>
2016-12-05 03:44:55 +0530

    Update DUPLICATE.py

--- DUPLICATE.py
+++ DUPLICATE.py
@@ -3,6 +3,8 @@ def findunique(a):
     for element in a:
         if a.count(element)==1:
             return element
+    else:
+        return 0
 
 if __name__ == '__main__':
     i = map(int,raw_input().split())
