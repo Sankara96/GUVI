5decac1634b7150462215e9b0ae4ae5ae4150f3b
Sankara96 <sankar.subramanian96@gmail.com>
2016-12-06 05:41:07 +0530

    Update reverse_words.py

--- reverse_words.py
+++ reverse_words.py
@@ -1,5 +1,8 @@
 import sys
-inp = raw_input().split()
-for elements in inp[::-1]:
-    sys.stdout.write(elements)
-    sys.stdout.write(" ")
+def reverse(inp):
+    for elements in inp[::-1]:
+        sys.stdout.write(elements)
+        sys.stdout.write(" ")
+if __name__ == '__main__':
+    inp = raw_input().split()
+    reverse(inp)
